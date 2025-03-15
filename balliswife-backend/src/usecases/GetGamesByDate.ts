4326c3781c5e31618b903294b18be6e60c55a626
domidomdomz <domz.ciriaco@gmail.com>
2025-03-15 11:16:39 +0800

    Remove GetGamesByDate

--- balliswife-backend/src/usecases/GetGamesByDate.ts
+++
@@ -1,9 +0,0 @@
-import { IRepository } from '../core/interfaces/IRepository';
-
-export class GetGamesByDate {
-    constructor(private repository: IRepository) { }
-
-    async execute(date: string): Promise<any> {
-        return this.repository.getGamesByDate(date);
-    }
-}
