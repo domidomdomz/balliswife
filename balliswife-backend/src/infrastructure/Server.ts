cced9e6ab0ed55c857167af7ef84b429412d81de
domidomdomz <domz.ciriaco@gmail.com>
2025-03-15 15:46:09 +0800

    Add cors

--- balliswife-backend/src/infrastructure/Server.ts
+++ balliswife-backend/src/infrastructure/Server.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from '../presentation/routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+
+// Allow requests from specific origins (e.g., React frontend)
+app.use(cors({
+    origin: 'http://localhost:3000', // The React app's origin
+}));
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
